4a75ec33d22aeaa21507b49f830312cc7a40eaf0
annaboinalaxman7-afk <annaboinalaxman7@gmail.com>
2025-10-26 16:00:29 +0530

    env updated from mogodb

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default async function connectToDB() {
   try {
-    await mongoose.connect('mongodb+srv://admin:admin123@nextj-portfolio.ptlpjvs.mongodb.net/');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Database connected successfully');
   } catch (e) {
     console.log(e);
